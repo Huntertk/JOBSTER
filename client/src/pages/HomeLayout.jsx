bd930f3b21b04adceb6503591ac8e9307f373c3e
Huntertk <taufik.khan65@gmail.com>
2024-06-09 23:27:30 +0530

    Nested Routes and Setting Layout

--- client/src/pages/HomeLayout.jsx
+++ client/src/pages/HomeLayout.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import {Outlet} from 'react-router-dom';
 
 const HomeLayout = () => {
   return (
-    <div>HomeLayout</div>
+    <div>
+      <nav>navbar</nav>
+      <Outlet />
+    </div>
   )
 }
 
