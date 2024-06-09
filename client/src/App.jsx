397d24400abf679800dd4bcbdc80d41afaf527d6
Huntertk <taufik.khan65@gmail.com>
2024-06-09 21:05:14 +0530

    React Router - Initial Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,21 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <h1>Hello</h1>
+  },
+  {
+    path:"/about",
+    element: <h1>About</h1>
+  },
+])
+
 const App = () => {
   return (
-    <h1>Jobify App</h1>
+    <div>
+      <RouterProvider router={router}/>
+    </div>
   )
 }
 
