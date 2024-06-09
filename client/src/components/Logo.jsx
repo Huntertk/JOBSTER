6d5c19a42fc962351348a4bc605a248ff9a147f4
Huntertk <taufik.khan65@gmail.com>
2024-06-09 16:17:06 +0530

    Initial commit for re development

--- client/src/components/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import logo from '../assets/images/logo.svg'
-const Logo = () => {
-  return (
-    <img src={logo} alt="logo" className='logo' />
-  )
-}
-
-export default Logo
