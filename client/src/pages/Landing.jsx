7812720080dd9e526a368979b55493ebd0e553ea
Huntertk <taufik.khan65@gmail.com>
2024-06-10 12:59:37 +0530

    exploring styled components

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import styled from 'styled-components'
+
+const StyledBtn = styled.button`
+  background-color: red;
+  font-size: 1.5rem;
+  color: #fff;
+`
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <div>
+      <h1>Landing Page</h1>
+      <StyledBtn>Button</StyledBtn>
+    </div>
   )
 }
 
