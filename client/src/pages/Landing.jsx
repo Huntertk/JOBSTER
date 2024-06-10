94589732d82bfa51251291a54db92e02fc8918ac
Huntertk <taufik.khan65@gmail.com>
2024-06-10 14:46:46 +0530

    Landing Page created using styled componenets

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,23 +1,24 @@
-import styled from 'styled-components'
+import Wrapper from "../assets/wrappers/LandingPage";
+import logo from '../assets/images/logo.svg';
+import main from '../assets/images/main.svg';
+import { Link } from "react-router-dom";
 
 
-const Wrapper = styled.div`
-  background-color: red;
-  h1{
-    font-weight: 800;
-    color: #ffffff;
-  }
-  .content{
-    background-color: blue;
-  }
-`
 
 const Landing = () => {
   return (
     <Wrapper>
-      <h1>Landing Page</h1>
-      <div className="content">
-        some content
+      <nav>
+        <img src={logo} alt="Jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>job <span>tracking</span> app</h1>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione cupiditate laboriosam aliquam error ea. Debitis, doloribus odit provident voluptatibus et esse iste voluptates repellendus molestias culpa molestiae necessitatibus possimus in.</p>
+          <Link to="/register" className="btn register-link">Register</Link>
+          <Link to="/login" className="btn">Login / demo</Link>
+        </div>
+        <img src={main} alt="Jobify" className="img main-img" />
       </div>
     </Wrapper>
   )
