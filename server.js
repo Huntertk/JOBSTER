778bb7b17f19c08aa8d1c8ed19733713f057b5e8
Huntertk <taufik.khan65@gmail.com>
2024-06-13 22:15:30 +0530

    Creating Edit and Delete Job

--- server.js
+++ server.js
@@ -57,6 +57,38 @@ app.get('/api/v1/jobs/:id', (req, res) => {
   res.status(200).json({ job });
 });
 
+// EDIT JOB
+app.patch('/api/v1/jobs/:id', (req, res) => {
+  const { id } = req.params;
+  const {company, position} = req.body;
+
+  if(!company || !position){
+    return res.status(400).json({messgae:"Please Provide value"})
+  }
+
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+  job.company = company
+  job.position = position
+  res.status(200).json({ msg: 'job modified', job });
+});
+
+// DELETE JOB
+
+app.delete('/api/v1/jobs/:id', (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+  const newJobs = jobs.filter((job) => job.id !== id);
+  jobs = newJobs;
+
+  res.status(200).json({ msg: 'job deleted' });
+});
+
 //Server Listining...
 app.listen(PORT, () => {
     console.log(`server running on PORT ${PORT}....`);
