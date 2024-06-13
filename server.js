6153648c44af7cce79a25117b276324337434a2a
Huntertk <taufik.khan65@gmail.com>
2024-06-13 15:16:40 +0530

    Get All Jobs

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import morgan from 'morgan';
+import { nanoid } from 'nanoid';
+
+
+let jobs = [
+    { id: nanoid(), company: 'apple', position: 'front-end' },
+    { id: nanoid(), company: 'google', position: 'back-end' },
+];
 
 //Express App Initialization
 const app = express();
 
 //PORT
 const PORT = process.env.PORT || 4000
+
 
 //Middlewares
 app.use(express.json())
@@ -21,6 +29,13 @@ app.post('/', (req, res) => {
     res.json({messgae:"Body Recieved", data: req.body})
 })
 
+  
+app.get('/api/v1/jobs', (req, res) => {
+  res.status(200).json({ jobs });
+});
+
+
+//Server Listining...
 app.listen(PORT, () => {
     console.log(`server running on PORT ${PORT}....`);
 })
